27f58a8836e6f41e790df971693c579325333b8c
Stefan Wiedemann <wistefan@googlemail.com>
2023-11-23 16:02:18 +0100

    fix tests

--- services/src/main/java/org/keycloak/protocol/oidc4vp/model/Role.java
+++ services/src/main/java/org/keycloak/protocol/oidc4vp/model/Role.java
@@ -2,11 +2,13 @@ package org.keycloak.protocol.oidc4vp.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.Set;
 
 @AllArgsConstructor
 @Data
+@NoArgsConstructor
 public class Role {
 
 	private Set<String> names;
